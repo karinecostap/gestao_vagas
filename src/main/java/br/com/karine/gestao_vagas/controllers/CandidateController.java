1236083a28b00bfd2ec80debf5782f83d04106bb
Karine <contato.karinecostap@gmail.com>
2025-11-11 11:03:53 -0300

    Primeiro commit: Estrutura inicial do projeto

---
+++ src/main/java/br/com/karine/gestao_vagas/controllers/CandidateController.java
@@ -0,0 +1,29 @@
+package br.com.karine.gestao_vagas.controllers;
+
+import br.com.karine.gestao_vagas.candidate.CandidateEntity;
+import br.com.karine.gestao_vagas.candidate.CandidateRepository;
+import br.com.karine.gestao_vagas.exceptions.UserFoundException;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/candidate")
+public class CandidateController {
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    @PostMapping("/")
+    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        this.candidateRepository
+                .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
+                .ifPresent((user) -> {
+                    throw new UserFoundException();
+                });
+        return this.candidateRepository.save(candidateEntity);
+    }
+}
